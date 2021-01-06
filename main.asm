345507f3afb6c0dc63b2362c5c060a0878ccd462
yasser1133 <47793179+yasser1133@users.noreply.github.com>
2021-01-06 07:11:58 +0200

    Add files via upload

--- main.asm
+++ main.asm
@@ -1,49 +1,60 @@
-include    G:\IrvineAss\Irvine\Irvine32.inc
-includelib G:\IrvineAss\Irvine\irvine32.lib
-includelib G:\IrvineAss\Irvine\kernel32.lib
-includelib \masm32\lib\user32.lib
+include    c:\Irvine\Irvine32.inc
+includelib c:\Irvine\irvine32.lib
+includelib c:\Irvine\kernel32.lib
+includelib y:\masm32\lib\user32.lib
 
-		; .data is used for declaring and defining variables
 
 .DATA
 
     arrayptr    DWORD OFFSET array
     array       BYTE 4096 DUP (?)
-    mes1        BYTE 10, "1-add number 2-display all numbers 3-remove a number 4-search for a number 5-search for a name 6-quit", 0
+    mes1        BYTE 10, "1-add number 2-display all numbers 3-remove a number 4-search for a number 5-quit", 0
     check byte 0,0
-    ; mes1        BYTE 10, "press 1 to add an element, 2 to print, 3 to quit    ", 0
+    ;mes1        BYTE 10, "press 1 to add an element, 2 to print, 3 to quit    ", 0
     yourName  byte "Name : ",0
     number byte "Number : ",0
-    mesToTakeName byte "Enter Your Name !",0
-    mesToTakeNumber byte "Enter Your Number !",0
-    msgMoreNumber byte "More Numbers? (y) or (n)" ,0
-    numToSearch byte "Enter The Number",0
-    num        DWORD  0,0
-    msgNumFound  byte "Number is Found",0
-    msgNumNotFound  byte "Number is not Found",0
+    mesToTakeName byte "Enter Your Name : ",0
+    mesToMaxNumber byte "Max Numbers !!!",0
+    mesToTakeNumber byte "Enter Your Number : ",0
+    msgMoreNumber byte "More Numbers? (y) or ??",0
+    maxNum byte 0,0
+    zeros  byte ?
+    eqaa byte "++",0
+
+      
+
 
 
 .CODE
 
 
 readin PROC
+    mov    maxNum , 0
+    mov edx, arrayptr           ; Argument for ReadString: Pointer to memory
+    mov al , eqaa
+    mov  [edx], al
+    lea edx, [edx+1+1]        ; EDX += EAX + 1
+    mov arrayptr, edx           ; New pointer, points to the byte where the next string should 
     lea   edx, mesToTakeName
 	call  writeString
 	call	CrLf
     mov edx, arrayptr           ; Argument for ReadString: Pointer to memory
-    mov ecx, 10                 ; Argument for ReadString: maximal number of chars
+    mov ecx, 20                 ; Argument for ReadString: maximal number of chars
     call ReadString             ; Doesn't change EDX
     test eax, eax               ; EAX == 0 (got no string)
     jz done                     ; Yes: don't store a new arrayptr
     lea edx, [edx+eax+1]        ; EDX += EAX + 1
     mov arrayptr, edx           ; New pointer, points to the byte where the next string should begin
     take_number:
+            cmp   maxNum , 3
+            je    maxNumFunc
             lea   edx, mesToTakeNumber
 	        call  writeString
 	        call	CrLf
             mov edx, arrayptr           ; Argument for ReadString: Pointer to memory
-            mov ecx, 10                 ; Argument for ReadString: maximal number of chars
+            mov ecx, 15                 ; Argument for ReadString: maximal number of chars
             call ReadString             ; Doesn't change EDX
+            inc     maxNum
             test eax, eax               ; EAX == 0 (got no string)
             jz done                     ; Yes: don't store a new arrayptr
             lea edx, [edx+eax+1]        ; EDX += EAX + 1
@@ -55,10 +66,41 @@ readin PROC
 			mov check , al
 			cmp check , 79h
 			je  take_number
+			jmp done
+ 
+ filling: 
+            mov edx, arrayptr           ; Argument for ReadString: Pointer to memory
+            mov al , eqaa
+            mov  [edx], al
+            lea edx, [edx+1+1]        ; EDX += EAX + 1
+            mov arrayptr, edx           ; New pointer, points to the byte where the next string should begin
+			jmp done
+
+maxNumFunc:
+            lea   edx, mesToMaxNumber
+	        call  writeString
+	        call	CrLf
+	        call	CrLf
+            mov    maxNum , 0
+            jmp done
+
             
     done:
     ret
 readin ENDP
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     
 
@@ -67,8 +109,30 @@ readin ENDP
 
 
 
-
 print PROC
+    lea edx, array              ; Points to the first string
+    L1:
+    cmp edx, arrayptr           ; Does it point beyond the strings?
+    jae done                    ; Yes -> break
+    
+    call WriteString            ; Doesn't change EDX
+    call Crlf                   ; Doesn't change EDX
+
+    scan_for_null:
+    inc edx
+    cmp BYTE PTR [edx], 0       ; Terminating null?
+    jne scan_for_null           ; no -> next character
+    inc edx                     ; Pointer to next string
+
+    jmp L1
+
+    done:
+    ret
+print ENDP
+
+
+
+search PROC
     lea edx, array              ; Points to the first string
     L1:
     cmp edx, arrayptr           ; Does it point beyond the strings?
@@ -87,54 +151,32 @@ print PROC
 
     done:
     ret
-print ENDP
+search ENDP
 
 
-searchNumber PROC
-    lea   edx, numToSearch ; mov edx, offset numToSearch
-	call  writeString
-	call	CrLf
-    mov ecx, 10 
-    call ReadString  ;EDX has the number
-    ;MOV num , edx
-    lea EDI , array
-    MOV CX,4096 ;load counter
-     looping:
-    CMP edx , [EDI]
-    je FOUND
-    INC  EDI
-    DEC  cx
-    jmp looping
 
-
-    MOV EAX,OFFSET array ;address data
-    MOV ebx , 3
-    Div ebx
-    MOV EDI,EDX ;remainder moved to DI
-    MOV CX,2 ;load counter
-    XOR AL,AL ;clear AL
-    REPNE SCASB
-    STD ;auto-decrement
-
-    
-
-FOUND:
-    lea   edx, msgNumFound 
-	call  writeString
-	call	CrLf
-notFOUND:
-    lea   edx, msgNumNotFound 
-	call  writeString
-	call	CrLf
-searchNumber ENDP
 
 
 
 
 main PROC
+
+    ;mov edx, offset fileName
+    ;call createoutputfile
+    ;mov al , mesToTakeNumber
+    ;call Writetofile
+
+    ;cmp [mes1] , 1
+    ;jg add1
+    ;jl stop
+
+
+
 start:
+    
     lea edx, mes1
     call WriteString
+    call	CrLf
     call ReadDec
     cmp eax, 1
     je add1
@@ -160,10 +202,12 @@ print2:
     jmp next                    ; Just a better name than in the OP
 
 
+
 search2:
-    call  searchNumber
-    jmp next
-    
+    call search
+    jmp next                    ; Just a better name than in the OP
+
+
 
 next:                           ; Just a better name than in the OP
     jmp start
